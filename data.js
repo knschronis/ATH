ce73c4047985fd0e8453aeb52f2b296c2f1b775d
knschronis <kns.chronis@gmail.com>
2025-03-14 17:14:02 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,699 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-no-name-athens-00",
+      "name": "No Name Athens 00",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.4410326566241975,
+        "pitch": -0.44828844119679623,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3480980075988764,
+          "pitch": 0.5952304437033931,
+          "rotation": 0,
+          "target": "1-no-name-athens-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-no-name-athens-01",
+      "name": "No Name Athens 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.3371985058137845,
+        "pitch": 0.25274234407382856,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2780497339044956,
+          "pitch": 0.6921063349164953,
+          "rotation": 0,
+          "target": "0-no-name-athens-00"
+        },
+        {
+          "yaw": 1.1105992767806576,
+          "pitch": 0.41057768983555576,
+          "rotation": 0,
+          "target": "2-no-name-athens-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-no-name-athens-02",
+      "name": "No Name Athens 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.805001956002153,
+        "pitch": 0.3087357211988664,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8967199736406233,
+          "pitch": 0.15690847090972326,
+          "rotation": 0,
+          "target": "11-no-name-athens-11"
+        },
+        {
+          "yaw": 1.803821708855839,
+          "pitch": 0.40831442871792767,
+          "rotation": 6.283185307179586,
+          "target": "3-no-name-athens-03"
+        },
+        {
+          "yaw": 0.03199855734970747,
+          "pitch": 0.46837350362165253,
+          "rotation": 0,
+          "target": "1-no-name-athens-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-no-name-athens-03",
+      "name": "No Name Athens 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.8779054883908977,
+        "pitch": 0.28874905018544794,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1902649082950525,
+          "pitch": 0.5797027930287122,
+          "rotation": 0,
+          "target": "5-no-name-athens--12"
+        },
+        {
+          "yaw": -3.0188449121648873,
+          "pitch": 0.33298496587517334,
+          "rotation": 0,
+          "target": "4-no-name-athens-04"
+        },
+        {
+          "yaw": 2.4323134811551927,
+          "pitch": 0.2944170575754903,
+          "rotation": 0,
+          "target": "1-no-name-athens-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-no-name-athens-04",
+      "name": "No Name Athens 04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.48112617053863,
+        "pitch": 0.484131140483429,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7155325952374465,
+          "pitch": 0.29535910397371623,
+          "rotation": 0,
+          "target": "2-no-name-athens-02"
+        },
+        {
+          "yaw": -2.5623080157298794,
+          "pitch": 0.2916282594061421,
+          "rotation": 0,
+          "target": "3-no-name-athens-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-no-name-athens--12",
+      "name": "No Name Athens -12",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.851570797042589,
+        "pitch": 0.03646777202695084,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3292380541650637,
+          "pitch": 0.2189890460772581,
+          "rotation": 0,
+          "target": "6-no-name-athens--11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-no-name-athens--11",
+      "name": "No Name Athens -11",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.4572205821467037,
+        "pitch": 0.06745916838010046,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10111623606327669,
+          "pitch": 0.34968537738302885,
+          "rotation": 0,
+          "target": "7-no-name-athens--13"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-no-name-athens--13",
+      "name": "No Name Athens -13",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.31129908478761,
+        "pitch": 0.23984863809550383,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0169651133186832,
+          "pitch": 0.5349797155280172,
+          "rotation": 0,
+          "target": "8-no-name-athens--15"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-no-name-athens--15",
+      "name": "No Name athens -15",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.1107760577664969,
+        "pitch": 0.21878507994529528,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6864391645842325,
+          "pitch": 0.32221824226485296,
+          "rotation": 4.71238898038469,
+          "target": "9-no-name-athens--14"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-no-name-athens--14",
+      "name": "No name Athens -14",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.0824815854775647,
+        "pitch": 0.17555443942420368,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.805842915613253,
+          "pitch": 0.45935241882844835,
+          "rotation": 0,
+          "target": "10-no-name-athens--16"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-no-name-athens--16",
+      "name": "No Name Athens -16",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.2661930277183107,
+        "pitch": 0.20445027503066449,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6600478573661377,
+          "pitch": 0.505461657864382,
+          "rotation": 7.0685834705770345,
+          "target": "3-no-name-athens-03"
+        },
+        {
+          "yaw": 0.8205436954473608,
+          "pitch": 0.41718619329228446,
+          "rotation": 0,
+          "target": "5-no-name-athens--12"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-no-name-athens-11",
+      "name": "No Name Athens 11",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.2223517318009876,
+        "pitch": 0.2963800977956872,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4140514403699536,
+          "pitch": 0.46687237300784545,
+          "rotation": 5.497787143782138,
+          "target": "12-no-name-athens-12"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-no-name-athens-12",
+      "name": "No Name Athens 12",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.4711517908099703,
+        "pitch": 0.343644310609891,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.547534218501637,
+          "pitch": 0.33884228096403746,
+          "rotation": 0,
+          "target": "13-no-name-athens-13"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-no-name-athens-13",
+      "name": "No Name Athens 13",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.0678858289281568,
+        "pitch": 0.21258550027052614,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4441566579323846,
+          "pitch": 0.3941207352410441,
+          "rotation": 0,
+          "target": "14-no-name-athens-14"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-no-name-athens-14",
+      "name": "No Name Athens 14",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.1706858566921152,
+        "pitch": 0.16936432193295659,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8450740594251096,
+          "pitch": 0.3034939514926016,
+          "rotation": 0,
+          "target": "15-no-name-athens-15"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-no-name-athens-15",
+      "name": "No Name Athens 15",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.6915618823160408,
+        "pitch": 0.3204535902619625,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9183493579672977,
+          "pitch": 0.4742137965356097,
+          "rotation": 1.5707963267948966,
+          "target": "16-no-name-athens-10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-no-name-athens-10",
+      "name": "No Name Athens 10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.3590452087183227,
+        "pitch": 0.39338294950057495,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2441100344938683,
+          "pitch": 0.5982022251439503,
+          "rotation": 0,
+          "target": "2-no-name-athens-02"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "No Name Athens",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
